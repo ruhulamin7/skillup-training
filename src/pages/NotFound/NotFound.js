2ebc05976e2cd787324b5e6858dc8fcec3278ec2
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-10-04 19:40:23 +0600

    not found page done

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>Not Found ! 403 </h3>
+        <div className="not-found">
+            <h1 class="errorcode">ERROR 404</h1>
+            <div class="errortext">This area is forbidden. Turn back now!</div>
+            <Link to="/home" className="button-back">
+                <button className="btn btn-warning btn-back">Back to home</button>
+            </Link>
         </div>
     );
 };
